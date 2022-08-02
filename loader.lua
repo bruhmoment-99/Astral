02189c4ee572ebb0d48dbd94a9c4d66fd238cd1e
bruhmoment-99 <63133116+bruhmoment-99@users.noreply.github.com>
2022-08-02 17:52:15 -0500

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bruhmoment-99/Astral/main/astralware%20arsenal.lua"))()
+elseif game.PlaceId == 2788229376 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bruhmoment-99/Astral/main/dahood%20Astral%25Ware.lua"))()
+end
